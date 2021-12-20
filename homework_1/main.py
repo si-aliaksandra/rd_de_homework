c49725e3dbca18b5219b4af7cba2487b8df271b8
BUDAPEST\Aliaksandra_Sidarenk <aliaksandra_sidarenka@epam.com>
2021-12-20 15:12:34 +0100

    [AS] homework_1 update config

--- homework_1/main.py
+++ homework_1/main.py
@@ -7,26 +7,24 @@ from requests.exceptions import RequestException
 
 def main():
 
-    api_config = Config("./API.yaml").get_config()
-    auth_config = Config("./auth.yaml").get_config()
+    config = Config("./config.yaml").get_config()
 
-    url = api_config["url"]
-
-    path_to_dir = os.path.join('.', 'data', api_config["payload"]['date'])
+    url = config["url"]
+    path_to_dir = os.path.join('.', 'data', config['API']['payload']['date'])
     os.makedirs(path_to_dir, exist_ok=True)
 
     try:
-        request_token = requests.post(url + auth_config["endpoint"], headers=auth_config["headers"],
-                                      data=json.dumps(auth_config["payload"]))
+        request_token = requests.post(url + config['auth']['endpoint'], headers=config['auth']['headers'],
+                                      data=json.dumps(config['auth']['payload']))
         token = request_token.json()['access_token']
         print(token)
-        headers = api_config["headers"]
-        headers["authorization"] = headers["authorization"].format(token=token)
+        headers = config['API']['headers']
+        headers['authorization'] = headers['authorization'].format(token=token)
         # OR headers = {"content-type": "application/json", "authorization": "JWT " + token}
-        response = requests.get(url + api_config["endpoint"], headers=headers,
-                                data=json.dumps(api_config["payload"]))
+        response = requests.get(url + config['API']['endpoint'], headers=headers,
+                                data=json.dumps(config['API']["payload"]))
         response.raise_for_status()
-        with open(os.path.join(path_to_dir, api_config["payload"]['date']), 'w') as json_file:
+        with open(os.path.join(path_to_dir, config['API']['payload']['date']), 'w') as json_file:
             json.dump(response.json(), json_file)
     except RequestException:
         print("Connection Error")
